510de93918d0b0d48e0c420edb6b83505650c24f
Hendy Almeida <hendy.almeida@hotmail.com>
2021-03-25 14:40:49 -0300

    funções de serviços

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 const nomePetshop = "PETSHOP AVANADE";
 
 let pets = [{
@@ -53,6 +54,60 @@ const vacinarPet = pet => {
     }
 }
 
-console.log(pets[0]);
-vacinarPet(pets[0]);
-console.log(pets[0]);
+const campanhaVacina = () => {
+    console.log("Campanha de vacina 2020");
+    console.log("vacinando...");
+
+    let petVacinadosCampanha = 0;
+    for (let pet of pets) {
+        if (!pet.vacinado) {
+            vacinarPet(pet);
+            petVacinadosCampanha++;
+        }
+    }
+    console.log(`${petVacinadosCampanha} pets foram vaciados nessa campanha!`);
+};
+
+const adicionarPet = novoPet => {
+    if (typeof novoPet == "object" && validarDados(novoPet)) {
+        // adiciona o pet
+        novoPet.nome = String(novoPet.nome);
+        novoPet.idade = parseInt(novoPet.idade);
+
+        if (!novoPet.servicos) {
+            novoPet.servicos = [];
+        }
+
+        pets.push(novoPet);
+    } else {
+        console.log("Ops, insira um argumento valido!");
+    }
+}
+
+const darBanhoPet = pet => {
+    pet.servicos.push({
+        'serviço':'banho',
+        'data': moment().format('DD-MM-YYYY')
+    });
+    console.log(`${pet.nome} está de banho tomado!`);
+};
+
+const tosarPet = pet => {
+    pet.servicos.push({
+        'serviço':'tosa',
+        'data': moment().format('DD-MM-YYYY')
+    });
+    console.log(`${pet.nome} está com cabelinho na régua :)`);
+};
+
+const apararUnhasPet = pet => {
+    pet.servicos.push({
+        'serviço':'corte de unhas',
+        'data': moment().format('DD-MM-YYYY')
+    });
+    console.log(`${pet.nome} está de unhas aparadas!`);
+};
+
+
+darBanhoPet(pets[0]);
+console.log(pets[0])
