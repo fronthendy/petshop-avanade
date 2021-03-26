2f2c4bf8b9dbab634637f6807a65025a9735c94e
Hendy Almeida <hendy.almeida@hotmail.com>
2021-03-26 13:58:24 -0300

    aula sexta - parte 1

--- index.js
+++ index.js
@@ -1,48 +1,14 @@
 const moment = require('moment');
 const nomePetshop = "PETSHOP AVANADE";
+const bancoDados = require('./bancoDados.json');
 
-let pets = [{
-        nome: 'Costelinha',
-        tipo: 'cachorro',
-        idade: 5,
-        raca: 'Vira-lata',
-        peso: 3,
-        tutor: 'Doug',
-        contato: '(11) 98899-4545',
-        vacinado: false,
-        servicos: ['banho', 'tosa']
-    },
-    {
-        nome: 'Snoopy',
-        tipo: 'cachorro',
-        idade: 13,
-        raca: 'Beagle',
-        peso: 5,
-        tutor: 'Charlie Brown',
-        contato: '(11) 95656-4545',
-        vacinado: true,
-        servicos: ['banho']
-    },
-    {
-        nome: 'Satanas',
-        tipo: 'gato',
-        idade: 5,
-        raca: 'Vira-lata',
-        peso: 3,
-        tutor: 'Bruxa do 71',
-        contato: '(11) 97171-7171',
-        vacinado: true,
-        servicos: ['banho', 'corte de unha']
-    }
-];
+let pets = bancoDados.pets;
 
 const listarPets = () => {
-
     for (let pet of pets) {
         //template string
         console.log(`${pet.nome}, ${pet.idade}, ${pet.tipo}, ${pet.raca}`);
     }
-
 }
 
 const vacinarPet = pet => {
@@ -109,5 +75,6 @@ const apararUnhasPet = pet => {
 };
 
 
-darBanhoPet(pets[0]);
-console.log(pets[0])
+listarPets();
+
+// console.log(pets)
